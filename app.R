083926271cabcd0b048adbdccedae70955723158
Setfelix <felixsetor@gmail.com>
2023-03-02 23:57:37 -0700

    Added lollipop of amino acid changes on protein structure in gene tab

--- app.R
+++ app.R
@@ -12,7 +12,8 @@ library(tidyverse)
 library(maftools)
 #library(DT)
 
-# make relevant functions
+# make relevant functions, objects
+
 
 # Define UI for application
 ui <- fluidPage(
@@ -23,7 +24,7 @@ ui <- fluidPage(
     # Sidebar with file input 
     sidebarLayout(
         sidebarPanel(
-            fileInput(inputId = "maf_file", buttonLabel = "Upload...",
+            fileInput(inputId = "maf_file",  buttonLabel = "Upload...",
                       label = "Load MAF file", 
                       multiple = F, accept = ".maf", placeholder = "Please upload a MAF file"),
             selectInput(inputId = "sample", label = "Select tumor sample", choices = character(), multiple = F),
@@ -48,33 +49,37 @@ ui <- fluidPage(
                          fluidRow(
                             column(width = 6,
                                    h4("Sample summary"),
-                                   dataTableOutput("sample_summ") 
+                                   DT::dataTableOutput("sample_summ") 
                                    ),
                             column(width = 6,
                                    h4("Gene summary"),
-                                   dataTableOutput("gene_summ")
+                                   DT::dataTableOutput("gene_summ")
                             )
                          )
-                         
                 ),
-                tabPanel("Sample",
-                         br(),
-                         tableOutput("sample_table_summary"),
-                         br(),
-                         plotOutput("sample_ss_histplot"),
-                         br(),
-                         plotOutput("sample_ds_histplot")
-                ),
-                tabPanel("Gene")
+                tabPanel("Sample"
+                         ),
+                tabPanel("Gene",
+                         fluidRow(
+                             column(width = 12,
+                                    h4("Amino acid changes on protein structure"),
+                                    plotOutput("gene_lollipop"),
+                                    br(),
+                                    DT::dataTableOutput("gene_data")
+                             )
+                         ) 
+                )
             )
         , width = 9)
     )
 )
 
 # Define server logic 
+#shinyServer
 server <- function(input, output, session) {
+   
     
-    input_data<-reactive({
+    input_data_chk<-reactive({
         req(input$maf_file)
         
         #check file extension
@@ -87,55 +92,77 @@ server <- function(input, output, session) {
         #        )
     })
     
-    # list for reactive values
+    # list for reactive data values
     rv <- reactiveValues()
-
-    #load sample names into selectInput
+    
+    #input_data()
+    
     observeEvent(input$maf_file, {
-        freezeReactiveValue(input, "sample")
-        maf_df<-read.delim(input$maf_file$datapath, stringsAsFactors = F, comment.char = "#", header = T)
-        rv$maf<-maf_df
+        
+        
+        rv$maf<-read.delim(input$maf_file$datapath, stringsAsFactors = F, comment.char = "#", header = T)
+        # print(head(rv$maf))
+        #create maf object
+        rv$maf_obj<-read.maf(rv$maf)
         
         rv$batch_name<-str_remove(input$maf_file$name, pattern = fixed(".maf"))
-        #rv$samples<-rv$maf$
-        #maf_df$Tumor_Sample_Barcode
         
         #get sample names
-        updateSelectInput(inputId = "sample", choices = unique(rv$maf$Tumor_Sample_Barcode))
+        updateSelectInput(inputId = "sample", choices = unique(rv$maf_obj@data$Tumor_Sample_Barcode))
         #get gene names
-        updateSelectInput(inputId = "gene", choices = unique(rv$maf$Hugo_Symbol))
+        updateSelectInput(inputId = "gene", choices = unique(rv$maf_obj@data$Hugo_Symbol))
         #get variant classes
-        updateCheckboxGroupInput(inputId = "var_class", choices = unique(rv$maf$Variant_Classification))
+        updateCheckboxGroupInput(inputId = "var_class", choices = unique(rv$maf_obj@data$Variant_Classification))
         #get variant type
-        updateCheckboxGroupInput(inputId = "var_type", choices = unique(rv$maf$Variant_Type))
+        updateCheckboxGroupInput(inputId = "var_type", choices = unique(rv$maf_obj@data$Variant_Type))
         
         #maf summary plot
-        rv$maf_obj<-read.maf(rv$maf)
         output$batch_maf_summary <- renderPlot({
-            # draw the histogram with the specified number of bins
-            plotmafSummary(rv$maf_obj, addStat = 'median')
+           plotmafSummary(rv$maf_obj, addStat = 'median')
         })
         #oncoplot
         output$oncoplot <- renderPlot({
             oncoplot(maf = rv$maf_obj)
         })
         #sample summary
-        output$sample_summ<-DT::renderDataTable(
-            DT::datatable(getSampleSummary(x=rv$maf_obj), extensions = c("Buttons", "Scroller"), fillContainer = T,
+        output$sample_summ<-DT::renderDataTable({
+            DT::datatable(getSampleSummary(x=rv$maf_obj), extensions = c("Buttons"), fillContainer = T,
                           rownames = FALSE,
                       options = list(paging = TRUE, scrollX= TRUE, ordering = TRUE, scrollY= TRUE,
-                                     dom = 'BSfrtip', pageLength = 5,
-                                     buttons = c('copy', 'csv', 'excel')), class = 'display')
-        , server = FALSE)
+                                     dom = 'Bfrtip', pageLength = 5,
+                                     buttons = c('copy', 'csv', 'excel')), class = "display")}, server = FALSE)
         #gene summary
-        output$gene_summ<-DT::renderDataTable(
-            DT::datatable(getGeneSummary(x=rv$maf_obj), extensions = c("Buttons", "Scroller"), fillContainer = T,
+        #server = TRUE
+        output$gene_summ<-DT::renderDataTable({
+            DT::datatable(getGeneSummary(x=rv$maf_obj), extensions = c("Buttons"), fillContainer = T,
                           rownames = FALSE,
                           options = list(paging = TRUE, scrollX=TRUE, ordering = TRUE, pageLength = 5,
-                                         scrollY=TRUE, dom = 'BSfrtip', buttons = c('copy', 'csv', 'excel')),
-                          class = 'display')
-        , server = FALSE)
-        
+                                         scrollY=TRUE, dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')), 
+                          class = "display")}, server = FALSE)
+        #print(head(rv$maf))
+    })
+    #print(head(rv$maf))
+    # #Gene view features
+    observeEvent(input$gene, {
+        # current_maf<-reactive({
+        #     req(rv)
+        #     rv$maf_obj
+        #     })
+        #current_maf_obj<-reactive({rv$maf_obj()})
+        #head(current_maf_obj@data)
+        output$gene_lollipop<-renderPlot({lollipopPlot(maf = rv$maf_obj, gene = input$gene)})
+
+        # rv$gene_flt_data<-isolate(rv$maf_obj@data) %>% filter(Hugo_Symbol==input$gene,
+        #                                           Variant_Classification %in% input$var_class,
+        #                                           Variant_Type %in% input$var_type)
+        # #print(head(rv$gene_flt_data))
+        # output$gene_data<-DT::renderDataTable({
+        #     DT::datatable(x=rv$gene_flt_data, extensions = c("Buttons"), fillContainer = T,
+        #                   rownames = FALSE,
+        #                   options = list(paging = TRUE, scrollX=TRUE, ordering = TRUE, pageLength = 5,
+        #                                  scrollY=TRUE, dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),
+        #                   class = "display")}, server = FALSE)
+
     })
     
 }
